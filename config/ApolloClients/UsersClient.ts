0f5379e89786c7a307bfcd4aa7239ceff15e70a1
Bruno <brunolucas23@gmail.com>
2022-09-29 15:45:24 -0300

    Updated Apollo config 2

--- config/ApolloClients/UsersClient.ts
+++ config/ApolloClients/UsersClient.ts
@@ -4,7 +4,8 @@ const uri = process.env.GRAPHQL_URI//"http://localhost:3000/api/graphql"
 
 const userClient = new ApolloClient({
   uri,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  defaultOptions:{mutate:{fetchPolicy:"network-only"}}
 })
 
 export default userClient
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -8,7 +8,7 @@ const schema = await buildSchema({
   resolvers: [UserResolver],
 })
 
-const server = new ApolloServer({schema})
+const server = new ApolloServer({schema,debug: true})
 
 export const config = {
   api:{
